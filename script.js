63104c83a3e2ce850f953bc90bed08adf939eb60
Susana S Silva <susana_santos@id.uff.br>
2022-04-07 12:22:54 -0300

    rascunho

---
+++ script.js
@@ -0,0 +1,9 @@
+let numbers = Math.random();
+console.log(numbers);
+numbers = numbers.toString(16);
+console.log(numbers);
+numbers = numbers.substring(2, 8);
+console.log(numbers);
+
+color = '#' + numbers;
+console.log(color);
